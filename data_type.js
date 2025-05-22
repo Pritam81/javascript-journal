9809d6e9789f4a8eede7c5340ecd35a4167afb14
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-05-22 10:57:53 +0530

    use of type of to check the data type

---
+++ data_type.js
@@ -0,0 +1,7 @@
+console.log(typeof'Hello'); //string
+console.log(typeof 1); //number
+console.log(typeof 0.232); //number
+console.log(typeof false);//boolean
+console.log(typeof true);//b0olean
+console.log(typeof'');//string(Empty String)
+
