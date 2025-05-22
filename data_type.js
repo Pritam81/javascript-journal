5925be197a8ddc67b66d0039d383d9f3b3762ffd
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-05-22 11:38:14 +0530

    data type completed

--- data_type.js
+++ data_type.js
@@ -1,7 +1,30 @@
 console.log(typeof'Hello'); //string
+console.log(typeof`hello "Inside tilde"`) //tilde to enclosed string 
 console.log(typeof 1); //number
 console.log(typeof 0.232); //number
 console.log(typeof false);//boolean
 console.log(typeof true);//b0olean
 console.log(typeof'');//string(Empty String)
+console.log(typeof +'100')//+,- used to convert string to number
+console.log(typeof -'Hello')//type is number but is NAN not a number
+console.log(+'hello')//NAN
+console.log(typeof parseInt('100'));//convert string to number using parseInt
+console.log( parseInt('Hello'))//NAN
+console.log(typeof parseInt('hello'))// type is converted to number but is not a number(NAN)
+console.log(typeof  (100+''));//converting number to string by concatinating ''
+console.log(typeof ( 1+ '100'));//string
+console.log(1+'00');//100 as '' with + converts number to string
+let a;
+console.log(a);// undefined 
+console.log(typeof a);//undefined
+console.log(+null);//0
+console.log(typeof +null)//number
+console.log(typeof null);//object
+
+
+
+
+
+
+
 
