b93467312bd1a503bc9812bbd0d448919d11589c
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-05-23 11:21:30 +0530

    Variable using let keyword done

---
+++ variables.js
@@ -0,0 +1,18 @@
+console.log("Welcome back\nWe are learning variables in Javascript!");
+//variable using let keyword
+let userName='Pritam STP'
+console.log('The username is',userName);
+console.log('The datatype of userName is',typeof userName);//string
+let age= 22;
+console.log('Age is',age);
+console.log('The dataType of age is',typeof age);
+let isPritamHappy;
+isPritamHappy=true;
+console.log(isPritamHappy);
+//the type of value the variable hold its datatype changes with respect to that value
+age='twenty two'
+console.log('Age is ',age);
+console.log('The datatype of age is now',typeof age);
+//accessing the character of username
+console.log(userName[3],userName[2]);
+
