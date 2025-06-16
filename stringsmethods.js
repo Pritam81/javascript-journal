c6d10cbb407462c3c5b6b3ee270ecfa452816f47
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-06-16 11:25:27 +0530

    lower and uppercase methods

--- stringsmethods.js
+++ stringsmethods.js
@@ -1,10 +1,18 @@
 //This file is used to demonstrate various string methods in JavaScript.
-let str="Hello, World!";
+let str="Hello, World11!";
 //accessing characters in a string with index
 console.log(str[0]); // H
 console.log(str[7]); // W
 console.log(str[13]); // undefined (index out of bounds)
 //length of string(total number of characters)
 console.log(str.length); // 13
-
-
+// . after variable name displays all the methods available for string
+//uppercase and lowercase conversion
+console.log(str.toUpperCase()); // "HELLO, WORLD!"
+console.log(str.toLowerCase()); // "hello, world!"
+ // converting str to uppercase and storing it
+let upperstr = str.toUpperCase();
+console.log(upperstr); // "HELLO, WORLD!"
+// converting str to lowercase and storing it
+let lowerstr = str.toLowerCase();   
+console.log(lowerstr); // "hello, world!"
