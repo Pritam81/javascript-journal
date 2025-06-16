874c74bc137a8da195429cc33d74e26904de6824
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-06-16 11:03:32 +0530

    alert,confirm,prompt uses

--- dialog_boxes.js
+++ dialog_boxes.js
@@ -1,1 +1,5 @@
 alert("Here comes your dialog box!");
+let result= confirm("Do you want to proceed?") ;
+result?alert("You chose to proceed!"):alert("You chose not to proceed!");
+let name=prompt("Please enter your name:");
+console.log(name);
