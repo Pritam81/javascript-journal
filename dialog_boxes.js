8b25d3f91ea3fa5814020474b902047f50da011a
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-06-16 11:05:24 +0530

    comment added

--- dialog_boxes.js
+++ dialog_boxes.js
@@ -1,5 +1,5 @@
-alert("Here comes your dialog box!");
-let result= confirm("Do you want to proceed?") ;
+alert("Here comes your dialog box!");//  returns void
+let result= confirm("Do you want to proceed?") ;//returns boolean , true if ok else false
 result?alert("You chose to proceed!"):alert("You chose not to proceed!");
-let name=prompt("Please enter your name:");
+let name=prompt("Please enter your name:");//return string if user enters something, null if user clicks cancel
 console.log(name);
