45b21f3b0d9c1ac4647daf15d49044bb973a2e12
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-06-16 11:16:25 +0530

    done

---
+++ stringsmethods.js
@@ -0,0 +1,10 @@
+//This file is used to demonstrate various string methods in JavaScript.
+let str="Hello, World!";
+//accessing characters in a string with index
+console.log(str[0]); // H
+console.log(str[7]); // W
+console.log(str[13]); // undefined (index out of bounds)
+//length of string(total number of characters)
+console.log(str.length); // 13
+
+
